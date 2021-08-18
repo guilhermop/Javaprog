5a1afc98f9d28c6e815bf8e28e0a25f274400413
Guilherme Ferreira Rodrigues <50489169+guilhermop@users.noreply.github.com>
2021-08-18 16:30:05 -0300

    Add files via upload

---
+++ Tabuada.java
@@ -0,0 +1,16 @@
+public class tabuada{
+
+    public static void main(String[] args){
+        /*Guilherme Ferreira Rodrigues 050016677 */ 
+        int d=10;
+        int i=1;
+        int v= 0;
+    
+        while(v < 99){
+            v=d*i;
+            i++;
+            System.out.printf("\n %d",v);
+        }
+    
+        }
+    }
